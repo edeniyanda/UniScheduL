5ef949675c68363fd4d9683860e6e30e8a934278
Eden Iyanda <edeniyanda@gmail.com>
2025-03-30 22:30:45 +0100

    Fix STATIC_ROOT path to correctly reference the BASE_DIR for static files

--- unischedule_django/unischedule_django/settings.py
+++ unischedule_django/unischedule_django/settings.py
@@ -267,5 +267,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ✅ These MUST be defined unconditionally (not inside if DEBUG)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
